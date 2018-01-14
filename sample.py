3738bec2351ac7ce37d8f74e298e3020149f88f7
mk918 <m.k.salamander@gmail.com>
2018-01-15 00:07:44 +0900

    second commit

--- sample.py
+++ sample.py
@@ -1,1 +1,12 @@
+#! /usr/bin/python
+# -*- coding:utf-8 -*-
 
+if __name__ == '__main__':
+    f = open('before.txt', 'r')
+    fw = open('after.txt', 'w')
+    for row in f:
+        row = row.replace('-\n','')
+        row = row.replace('\n',' ')
+        fw.write(row)
+    f.close()
+    fw.close()
